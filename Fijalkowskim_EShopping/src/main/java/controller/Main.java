aa4f54f505078713e8fb765b4ac84fbc41dc94a4
Mateusz Fijałkowski <fijalkowskim01@gmail.com>
2023-10-19 10:47:21 +0200

    Documentation added

--- Fijalkowskim_EShopping/src/main/java/controller/Main.java
+++ Fijalkowskim_EShopping/src/main/java/controller/Main.java
@@ -1,6 +1,13 @@
 package controller;
-
+/**
+ * Main class of the application.
+ * @author Fijalkowskim
+ * @version %I%, %G%
+ */
 public class Main {
+    /**
+     * Main method interacting only with Controller class.
+     */
     public static void main(String[] args)
     {
         Controller controller = new Controller();
